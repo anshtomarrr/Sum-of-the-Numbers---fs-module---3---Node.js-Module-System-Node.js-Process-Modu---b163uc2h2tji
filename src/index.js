f9792ce316795896aa56a8a11662c7648845d1a4
Ansh tomar <ansh.tomar2024@nst.rishihood.edu.in>
2025-08-21 12:46:32 +0530

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const data = fs.readFileSync('input.txt', 'utf8');
+let x = data.split('\n')
+
+let ans = 0;
+x.map((e)=>{
+  let a = e.split(' ');
+  ans+= parseInt(a[1]);
+})
+console.log(ans);
+fs.writeFileSync('output.txt', ans.toString(), 'utf8');
