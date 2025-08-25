38ac3dbda87b935005bf4b8ee7b6d1bd59debad4
Ansh tomar <ansh.tomar2024@nst.rishihood.edu.in>
2025-08-25 15:41:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
-const data = fs.readFileSync('input.txt', 'utf8');
+const inputPath = path.join(__dirname, "input.txt");   // input.txt in same folder as index.js
+const outputPath = path.join(__dirname, "output.txt"); // output.txt will be created here
+const data = fs.readFileSync(inputPath, 'utf8');
 let x = data.trim().split('\n')
 
 let ans = 0;
@@ -8,4 +10,5 @@ x.map((e)=>{
   ans+= parseInt(a[1]);
 })
 console.log(ans);
-fs.writeFileSync('output.txt', ans.toString());
+fs.writeFileSync(outputPath, ans.toString());
+
