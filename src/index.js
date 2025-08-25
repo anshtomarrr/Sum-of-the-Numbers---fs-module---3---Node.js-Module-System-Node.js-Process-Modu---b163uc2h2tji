609c73351bc09e48d9a91f6ba278702df7b1ee68
Ansh Tomar <anshtomarrr@gmail.com>
2025-08-25 16:07:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-const fs = require('fs');
-const inputPath = path.join(__dirname, "input.txt");   // input.txt in same folder as index.js
-const outputPath = path.join(__dirname, "output.txt"); // output.txt will be created here
-const data = fs.readFileSync(inputPath, 'utf8');
-let x = data.trim().split('\n')
+const fs = require("fs");
+let input = __dirname + "/input.txt";
+let output = __dirname + "/output.txt";
+const data = fs.readFileSync(input, "utf8").trim();
+let lines = data.split("\n");
 
 let ans = 0;
-x.map((e)=>{
-  let a = e.split(' ');
-  ans+= parseInt(a[1]);
-})
-console.log(ans);
-fs.writeFileSync(outputPath, ans.toString());
 
+lines.forEach((line) => {
+  let parts = line.split(" ");
+  ans += parseInt(parts[1]);
+});
+
+console.log(ans);
+fs.writeFileSync(output, ans.toString());
